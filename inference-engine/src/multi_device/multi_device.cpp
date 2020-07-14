106d7dc57fd79c599fa4a4a8614f4fa681dea39c
myshevts <maxim.y.shevtsov@intel.com>
2020-07-14 17:34:52 +0300

    counters within the multi

--- inference-engine/src/multi_device/multi_device.cpp
+++ inference-engine/src/multi_device/multi_device.cpp
@@ -153,6 +153,7 @@ MultiDeviceExecutableNetwork::MultiDeviceExecutableNetwork(const DeviceMap<Infer
     for (auto&& networkValue : _networksPerDevice) {
         auto& device  = networkValue.first;
         auto& network = networkValue.second;
+        _statsPerDevice.insert({device, 0});
 
         auto itNumRequests = _devicePriorities.find(device);
         unsigned int optimalNum = 0;
@@ -203,6 +204,7 @@ void MultiDeviceExecutableNetwork::ScheduleToWorkerInferRequest() {
             IdleGuard idleGuard{workerRequestPtr, idleWorkerRequests};
             Task inferPipelineTask;
             if (_inferPipelineTasks.try_pop(inferPipelineTask)) {
+                _statsPerDevice[device.first]++;
                 _thisWorkerInferRequest = workerRequestPtr;
                 inferPipelineTask();
                 idleGuard.Release();
@@ -229,6 +231,13 @@ MultiDeviceExecutableNetwork::~MultiDeviceExecutableNetwork() {
      *       But AsyncInferRequest destructor should waits for all asynchronous tasks that are used by the request
      */
     _workerRequests.clear();
+    int sum = 0;
+    std::cout << std::endl;
+    for (auto device : _statsPerDevice) {
+        std::cout << device.first << " executed " << device.second << " requests" << std::endl;
+        sum += device.second;
+    }
+    std::cout << std::endl <<  "ALL executed" << sum << " requests" << std::endl;
 }
 
 InferenceEngine::InferRequestInternal::Ptr MultiDeviceExecutableNetwork::CreateInferRequestImpl(InferenceEngine::InputsDataMap networkInputs,
